88e2715d8726760c5079f8fc123d0244664178e3
marjanovicm13 <marjanovicm314@gmail.com>
2022-04-29 18:28:49 +0200

    Implementiran currency exchange calculator

--- src/app/web/pages/currency-calc/currency-calc.page.ts
+++ src/app/web/pages/currency-calc/currency-calc.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UseExistingWebDriver } from 'protractor/built/driverProviders';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-currency-calc',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrencyCalcPage implements OnInit {
 
+  inputValue: number;
+  outputValue: number;
+  inputCurrency: string;
+  outputCurrency: string;
+
+  hrkUsd: number = 7.17;
+  hrkEur: number = 7.56;
+  eurUsd: number = 1.05;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleEvent(){
+    console.log(this.inputCurrency);
+  }
+
+  calculateExchange(){
+    if(this.inputCurrency == "hrk" && this.outputCurrency == "usd"){
+      this.outputValue = this.inputValue / this.hrkUsd;
+    }
+    else if(this.inputCurrency == "usd" && this.outputCurrency == "hrk"){
+      this.outputValue = this.inputValue * this.hrkUsd;
+    }
+    else if(this.inputCurrency == "hrk" && this.outputCurrency == "eur"){
+      this.outputValue = this.inputValue / this.hrkEur;
+    }
+    else if(this.inputCurrency == "eur" && this.outputCurrency == "hrk"){
+      this.outputValue = this.inputValue * this.hrkEur;
+    }
+    else if(this.inputCurrency == "usd" && this.outputCurrency == "eur"){
+      this.outputValue = this.inputValue / this.eurUsd;
+    }
+    else if(this.inputCurrency == "eur" && this.outputCurrency == "usd"){
+      this.outputValue = this.inputValue * this.eurUsd;
+    }
+
   }
 
 }
